bfd9e1389417d0a5a5f6b471e5e932a6cdf28fe4
ryansummerlin <ryansummerlin777@gmail.com>
2023-04-19 18:55:26 -0700

    Added all of the search functions, all desired output is appearing in the console

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,31 +1,109 @@
 export function findElementById(id) {
     // Return the element in the DOM with corresponding `id`
-    for (let i = 0; i < document.body.children.length; i++) {
-        let child = document.body.children[i];
+    function checkId(array) {
+        for (let i = 0; i < array.length; i++) {
+            let el = array[i];
+            if (el.id === id) {
+                return el;
+            }
 
+            if (el.children) {
+                if (checkId(el.children)) {
+                    return checkId(el.children);
+                }
+             }
+        }
+
+        return false;
     }
+
+    return checkId(document.body.children);
 }
 
 export function findFirstElementOfTag(tag) {
     // Return the first occurence of an element of tag name `tag`
+    function findFirstTag(array) {
+        for (let i = 0; i < array.length; i++) {
+            let el = array[i];
+            if (el.tagName === tag) {
+                return el;
+            }
 
-    // Your code here
+            if (el.children) {
+                if (findFirstTag(el.children)) {
+                    return findFirstTag(el.children);
+                }
+            }
+        }
+
+        return false;
+    }
+
+    return findFirstTag(document.body.children);
 }
 
 export function findFirstElementOfClass(cls) {
     // Return the first occurence of an element of class `cls`
+    function findFirstClass(array) {
+        for (let i = 0; i < array.length; i++) {
+            let el = array[i];
+            if (el.className === cls) {
+                return el;
+            }
 
-    // Your code here
+            if (el.children) {
+                if (findFirstClass(el.children)) {
+                    return findFirstClass(el.children);
+                }
+            }
+        }
+
+        return false;
+    }
+
+    return findFirstClass(document.body.children);
 }
 
 export function findElementsOfTag(tag) {
     // Return an array of elements that have a tag name of `tag`
+    let tagElements = [];
+    function findTagElements(arr) {
+        for (let i = 0; i < arr.length; i++) {
+            let el = arr[i];
+            if (el.tagName === tag) {
+                tagElements.push(el);
+            }
 
-    // Your code here
+            if (el.children) {
+                findTagElements(el.children);
+            }
+        }
+
+        return;
+    }
+
+    findTagElements(document.body.children);
+    return tagElements;
 }
 
 export function findElementsOfClass(cls) {
     // Return an array of elements that have are of class `cls`
+    let classElements = [];
+    function findClassElements(arr) {
+        for (let i = 0; i < arr.length; i++) {
+            let el = arr[i];
+            if (el.className === cls) {
+                classElements.push(el);
+            }
 
-    // Your code here
+            if (el.children) {
+                findClassElements(el.children);
+            }
+        }
+
+        return;
+    }
+
+    findClassElements(document.body.children);
+    return classElements;
 }
